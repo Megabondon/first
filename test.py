f27a320b5325bab37e62710a7981b19acfd94107
Megabondon <bokunovvana@gmail.com>
2026-06-09 16:42:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from app import sum
 def test_sum():
     assert sum(2, 3) == 5, "2 + 3 should equal 5"
     assert sum(-1, 1) == 0, "-1 + 1 should equal 0"
-    print("All testes passed")
+    print("All tests passed")
 
 if _name_ == "_main_":
    test_sum()
